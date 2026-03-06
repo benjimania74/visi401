643a9aea0e200b97dd30e32b6937ad0e1b8dd850
Tom Hirschowitz <tom.hirschowitz@univ-smb.fr>
2026-03-06 18:30:01 +0100

    un meilleur pp_valeur

--- bin/main.ml
+++ bin/main.ml
@@ -20,3 +20,21 @@ let () = repl ()
 (* let lexbuf = Lexing.from_string input in *)
 (* let result = Parser.main Lexer.read lexbuf in *)
 (* Printf.printf "Res: %s\n" (show_term result) *)
+
+(* (λ.(λ.(1 (1 (1 0))))) (λ.(λ.(1 (1 0)))) (λ.0)  *)
+(* donne *)
+(* 
+   λ.1; 1; 0; @; @; ret 
+   [ λ.1; 1; 0; @; @; ret 
+      [ λ.1; 1; 0; @; @; ret 
+         [ λ.0; ret [  ]  ]
+      ]  
+   ] 
+ *)
+(* 
+   λx. f (f x) [ f ↦ 
+                 λx. g (g x) [ g ↦ 
+                                 λx. h (h x) [ h ↦ 
+                                               λx.x ] ] ]
+ie on double trois fois la fonction, c'est ok, mais pas super frappant…
+ *)
--- lib/cam.ml
+++ lib/cam.ml
@@ -26,11 +26,26 @@ valeur = Code of instruction list | Env of env | Closure of instruction list * e
 (*   | Some v -> "Some (" ^ show_valeur v ^ ")" *)
 
 
+let rec pp_valeur = function 
+    | Code instrs -> "⟨" ^ (pp_instrs instrs) ^ "⟩"
+    | Env env -> "{" ^ (pp_env env) ^ "}"
+    | Closure (instrs,env) -> 
+       "λ." ^ (pp_instrs instrs) ^ " [ " ^ (pp_env env) ^ " ] "
+    | Value n -> string_of_int n
+and pp_instrs instrs = String.concat "; " (List.map pp_instr instrs)
+and pp_instr = function 
+  | Access n -> string_of_int n
+  | MakeClosure instrs -> "Λ." ^ (pp_instrs instrs)
+  | Apply -> "@"
+  | Return -> "ret"
+and pp_env env = String.concat ", " (List.map pp_valeur env)
+
+
 let pp_option pp_value = function
   | None -> "None"
   | Some v -> "Some (" ^ pp_value v ^ ")"
 
-let pp_valeur_option = pp_option show_valeur
+let pp_valeur_option = pp_option pp_valeur
 
 
 type pile = valeur Stack.t;;
