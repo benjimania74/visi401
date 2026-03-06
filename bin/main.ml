5f1b0ccd5c588f37095fe86f336b0e2e511a3bde
Tom Hirschowitz <tom.hirschowitz@univ-smb.fr>
2026-03-06 17:34:03 +0100

    on se rabat sur main

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,24 @@
 open Visi
 open Lambda
+open Lexer
+open Parser
 
-let () =
-let input = "λ. 0 1 " in
-let lexbuf = Lexing.from_string input in
-let result = Parser.main Lexer.read lexbuf in
-Printf.printf "Res: %s\n" (show_term result)
+let repl () =
+  let lexbuf = Lexing.from_channel stdin in
+  while true do
+    Printf.printf "> %!";
+    let result = Parser.main Lexer.read lexbuf in
+    let value = Cam.runCam result in
+    Printf.printf "%s\n%!" (show_valeur value)
+  done
+
+let () = repl ()
+
+(* open Visi *)
+(* open Lambda *)
+
+(* let () = *)
+(* let input = "λ. 0 1 " in *)
+(* let lexbuf = Lexing.from_string input in *)
+(* let result = Parser.main Lexer.read lexbuf in *)
+(* Printf.printf "Res: %s\n" (show_term result) *)
