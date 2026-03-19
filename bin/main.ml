b100ea5806c958ed425a996b7e2c019696dc23bc
Tom Hirschowitz <tom.hirschowitz@univ-smb.fr>
2026-03-19 17:04:05 +0100

    quelques exemples

--- bin/main.ml
+++ bin/main.ml
@@ -21,20 +21,21 @@ let () = repl ()
 (* let result = Parser.main Lexer.read lexbuf in *)
 (* Printf.printf "Res: %s\n" (show_term result) *)
 
-(* (λ.(λ.(1 (1 (1 0))))) (λ.(λ.(1 (1 0)))) (λ.0)  *)
-(* donne *)
-(* 
-   λ.1; 1; 0; @; @; ret 
-   [ λ.1; 1; 0; @; @; ret 
-      [ λ.1; 1; 0; @; @; ret 
-         [ λ.0; ret [  ]  ]
-      ]  
-   ] 
- *)
-(* 
-   λx. f (f x) [ f ↦ 
-                 λx. g (g x) [ g ↦ 
-                                 λx. h (h x) [ h ↦ 
-                                               λx.x ] ] ]
-ie on double trois fois la fonction, c'est ok, mais pas super frappant…
- *)
+
+              (* Exemple d'exponentielle 2**3 *)
+(* (λ.(λ.(1 (1 (1 0))))) (λ.(λ.(1 (1 0)))) S Z  *)
+
+              (* Exemple d'exponentielle 3**2 *)
+(* (λ.(λ.(1 (1 0)))) (λ.(λ.(1 (1 (1 0))))) S Z *)
+
+              (* Exemple de multiplication 3*2 *)
+(* (λ.(λ.(1 (1 0)))) ((λ.(λ.(1 (1 (1 0))))) S) Z *)
+
+              (* Exemple de multiplication 2*3 *)
+(* (λ.(λ.(1 (1 (1 0))))) ((λ.(λ.(1 (1 0)))) S) Z *)
+
+              (* Exemple d'addition 2+3 *)
+(* ((λ.(λ.(1 (1 (1 0))))) S) (((λ.(λ.(1 (1 0)))) S) Z) *)
+
+              (* Exemple d'addition 3+2 *)
+(* ((λ.(λ.(1 (1 0)))) S) (((λ.(λ.(1 (1 (1 0))))) S) Z) *)
